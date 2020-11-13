777b091e6e331580d57ecc0ce741ad06bf8c44fb
dolow <velvet.nanashi@gmail.com>
2020-11-13 10:32:56 +0900

    added constatnts.go, go.mod, go.sum

---
+++ constants.go
@@ -0,0 +1,17 @@
+package nestedtext
+
+const (
+	DictionaryKeySeparator = ':'
+	ListSymbol             = '-'
+	TextSymbol             = '>'
+	CommentSymbol          = '#'
+	IndentChar             = ' '
+	Space                  = ' '
+	Tab                    = '\t'
+	LineBreak              = '\n'
+	Quote                  = '\''
+	DoubleQuote            = '"'
+
+	EmptyChar     byte = 0x00
+	NotFoundIndex int  = -1
+)
---
+++ go.mod
@@ -0,0 +1,8 @@
+module https://github.com/dolow/nt-go
+
+go 1.14
+
+require (
+	github.com/stretchr/testify v1.6.1
+	golang.org/x/tools v0.0.0-20201112185108-eeaa07dd7696 // indirect
+)
